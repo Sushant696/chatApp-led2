f9941a69cc561ee2b1ff14666f4101ebfd624507
sushant696 <sushantprasaii7@gmail.com>
2024-07-12 17:39:53 +0545

    feat: import fixes

--- src/DAO/FriendShip/FriendshipDAO.java
+++ src/DAO/FriendShip/FriendshipDAO.java
@@ -11,7 +11,7 @@ package DAO.FriendShip;
 import Model.User;
 import java.util.List;
 
-interface FriendshipDAO {
+public interface FriendshipDAO {
         void sendFriendRequest(int userId, int friendId);
     void respondToFriendRequest(int friendshipId, String status);
     List<User> getFriendsList(int userId);
